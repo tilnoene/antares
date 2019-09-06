2204e5811686d3fe2c0c21b223a472cd634831db
Victor <45538072+tilnoene@users.noreply.github.com>
2019-09-06 12:54:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+/*
+	Introspect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+dados = [[137, "Luciano Barreto", "3A", 98, 0, []],
+         [666, "Tio Demon", "2B", 23, 2, []]]
+
+function carregaValores(){
+	for (var i in dados){
+		escreveLinhas(i, dados)
+	}
+}
+		 
+function escreveLinhas(i, dados){
+	var table = document.getElementById("tabela");
+	var row = table.insertRow(-1);
+	
+	var posicao = row.insertCell(0);
+	var nome = row.insertCell(1);
+	var turma = row.insertCell(2);
+	var pontos = row.insertCell(3);
+	
+	posicao.innerHTML = String(Number(i)+1)+".";
+	nome.innerHTML = dados[i][1]; // Sem o ID
+	turma.innerHTML = dados[i][2];
+	pontos.innerHTML = dados[i][3];
+}
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Panel Toggle.
+				$('<a href="#navPanel" class="navPanelToggle"></a>')
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						$('#nav').html() +
+						'<a href="#navPanel" class="close"></a>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left'
+					});
+
+			// Fix: Remove transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navPanel')
+						.css('transition', 'none');
+
+	});
+
+})(jQuery);
